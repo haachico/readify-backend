9c606d227810e1d7a844f5594fdfb5023bb47c1d
Nilesh Kokare <nile10kokare@gmail.com>
2026-04-12 21:07:20 +0530

    changes

--- backend/src/utils/emailService.js
+++ backend/src/utils/emailService.js
@@ -3,7 +3,6 @@ const Logger = require('./logger');
 require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
   host: 'smtp.gmail.com',
   port: 587,
   secure: false, // false for TLS (STARTTLS)
