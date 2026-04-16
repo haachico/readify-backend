244a43d922fc7204538f2384b2f50c5d434785ab
Nilesh Kokare <nile10kokare@gmail.com>
2026-04-16 09:22:31 +0530

    changes

--- backend/src/utils/googleSheets.js
+++ backend/src/utils/googleSheets.js
@@ -22,7 +22,7 @@ const initGoogleSheets = async () => {
             private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
             private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
             client_email: process.env.GOOGLE_CLIENT_EMAIL,
-            client_id: process.env.GOOGLE_CLIENT_ID,
+            client_id: process.env.GOOGLE_SHEETS_CLIENT_ID,
             auth_uri: process.env.GOOGLE_AUTH_URI,
             token_uri: process.env.GOOGLE_TOKEN_URI,
             auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
